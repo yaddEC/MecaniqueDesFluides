7196282456594e94e2fa704d5a69d4dc5483b217
Yann DECHAUX <y.dechaux@student.isartdigital.com>
2023-05-05 14:54:24 +0200

    [FIX] Fixed prefabs relativve pos

--- Assets/Script/WaterTower.cs
+++ Assets/Script/WaterTower.cs
@@ -15,10 +15,23 @@ public class WaterTower : MonoBehaviour
     public GameObject HorBottom;
     public GameObject Support;
     public GameObject Hole;
+
+    private Vector3 PosVerRight;
+    private Vector3 PosVerLeft;
+    private Vector3 PosHorTop;
+    private Vector3 PosHorBottom;
+    private Vector3 PosHole;
+    private Vector3 PosSupport;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PosVerRight = VerRight.transform.localPosition;
+        PosVerLeft = VerLeft.transform.localPosition;
+        PosHorTop = HorTop.transform.localPosition;
+        PosHorBottom = HorBottom.transform.localPosition;
+        PosHole = Hole.transform.localPosition;
+        PosSupport = Support.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -33,19 +46,19 @@ public class WaterTower : MonoBehaviour
     {
         Hole.transform.localScale = new Vector3(Hole.transform.localScale.x, holeSize, Hole.transform.localScale.z);
         Support.transform.localScale = new Vector3(0.3f, supportHeight, 1);
-        VerLeft.transform.position = new Vector3(-width * 0.5f-0.1f, supportHeight * 0.5f + (height * 0.5f), 0);
+        VerLeft.transform.localPosition = new Vector3(PosVerLeft.x - (width * 0.5f) - 0.1f + 1.25f, PosVerLeft.y + Support.transform.localScale.y * 0.5f + (height * 0.5f)-6 , 0);
         VerLeft.transform.localScale = new Vector3(0.3f, height, 1);
-        VerRight.transform.position = new Vector3(width * 0.5f+0.1f, supportHeight * 0.5f + (height * 0.5f), 0);
-        Hole.transform.position = new Vector3(width * 0.5f + 0.1f, supportHeight * 0.5f +0.3f+ Hole.transform.localScale.y*0.5f, 0);
+        VerRight.transform.localPosition = new Vector3(PosVerRight.x + width * 0.5f + 0.1f - 1.25f, PosVerRight.y + Support.transform.localScale.y * 0.5f + (height * 0.5f)-6 , 0);
+        Hole.transform.localPosition = new Vector3(VerRight.transform.localPosition.x, PosHole.y - (Support.transform.localScale.y * 0.5f - 0.3f - Hole.transform.localScale.y * 0.5f)-0.244f, 0);
         VerRight.transform.localScale = new Vector3(0.3f, height, 1);
 
     }
 
     void HorizontalUpdate()
     {
-       HorTop.transform.position = new Vector3(HorTop.transform.position.x, supportHeight * 0.5f + height-0.15f, 0);
+       HorTop.transform.localPosition = new Vector3(PosHorTop.x,PosHorTop.y+supportHeight * 0.5f + height+0.15f-7, 0);
        HorTop.transform.localScale = new Vector3(width,0.3f, 1);
-       HorBottom.transform.position = new Vector3(HorBottom.transform.position.x, supportHeight*0.5f+0.15f, 0);
+       HorBottom.transform.localPosition = new Vector3(PosHorBottom.x, PosHorBottom.y+ supportHeight *0.5f-0.15f-5f, 0);
        HorBottom.transform.localScale = new Vector3(width, 0.3f, 1);
     }
 
